eefb7339b4e9eac86daaa3834708525761ea6568
jippey67 <jeroenwalta@gmail.com>
2019-12-04 14:47:32 +0100

    cleaning the code

--- index.js
+++ index.js
@@ -5,23 +5,15 @@ const port = 3000
 const { exec } = require('child_process');
 
 app.get('/:command', (req, res) => {
-
   let comm = req.params.command;
   exec(('~/komodo/src/komodo-cli '+comm), (err, stdout, stderr) => {
     if(err) {
       return;
     }
-    console.log(`stdout: ${stdout}`);
-    res.send(`answer: ${stdout}`);	
-
-
-
-
+    //console.log(`stdout: ${stdout}`);
+    res.send(`answer: ${stdout}`);
   });
   //res.send(answ);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
