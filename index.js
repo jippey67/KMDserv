d9878d0150a9c106cd1156a3cc792f3a6353abb7
jippey67 <jeroenwalta@gmail.com>
2019-12-11 08:22:46 +0100

    JSON test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const { exec } = require('child_process');
-
+/*
 app.get('/:command', (req, res) => {
   let comm = req.params.command;
   exec(('~/komodo/src/komodo-cli '+comm), (err, stdout, stderr) => {
@@ -15,18 +15,18 @@ app.get('/:command', (req, res) => {
   });
   //res.send(answ);
 });
+*/
 
-app.get('/richlist:n', (req, res) => {
-  let topn = req.params.n;
-  let toDo = '~/komodo/src/komodo-cli -ac_name=CCL getsnapshot '+topn;
-  console.log(`executing ${toDo}`);
-  exec((toDo), (err, stdout, stderr) => {
+app.get('/:command', (req, res) => {
+  let comm = req.params.command;
+  exec(('~/komodo/src/komodo-cli '+comm), (err, stdout, stderr) => {
     if(err) {
       return;
     }
-    res.send(`answer: ${stdout}`);
+    //console.log(`stdout: ${stdout}`);
+    res.send(stdout);
   });
+  //res.send(answ);
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
